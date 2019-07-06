d93e55c67ce9a66fabf45f32a3ed03ba5e4e9a88
igasm <iga.smarz@gmail.com>
2019-07-06 13:12:59 +0200

    new components creation, component templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ServerComponent ],
+  declarations: [ AppComponent, HelloComponent, ServerComponent, ServersComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
